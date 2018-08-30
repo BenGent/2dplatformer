fb16597275b62f19b2954294f98dd25c7e8b16c3
Вениамин <ben.gentsis@gmail.com>
2018-08-30 13:06:41 +0300

    начали делать физику

---
+++ Assets/scripts/MovePlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 27f;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movex = input.getaxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * movex * speed * Time.deltaTime);
+    }
+}
